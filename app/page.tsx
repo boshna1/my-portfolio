337fa426a2f137fbc298b5f17aac0abaf1b7c86b
boshna1 <joshua.dinata@ontariotechu.net>
2026-06-23 21:01:33 -0400

    3rd

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,18 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main>
-      <h1>Joshua Dinata's Portfolio</h1>
-      <p>Under Construction</p>
+      <div className="Main-Screen">
+        <div className="Main-Title">
+          <h1>Joshua Dinata's Portfolio</h1>
+          <p>Under Construction</p>
+        </div>
+        <div className="Subsection">
+          <h2>Profile</h2>
+          <h2>Projects</h2>
+          <h2>Experience</h2>
+          <h2>Skills</h2>
+        </div>
+      </div>
     </main>
   );
 }
